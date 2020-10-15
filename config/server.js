f0579ada64e5cafeff920b49416b903ab6079553
Juan Carlos Arroyo Sosa <juan.arroyo@comisiondelaverdad.co>
2020-10-15 10:27:56 -0500

    carga inicial separación repos front y back

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5981ddb435e415f07dc792c0d8127b25'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a2b7eddb-93d3-44e6-9c94-f1745a6b8f6d'
+};
